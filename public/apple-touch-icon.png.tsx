f6439b295d517e78ae1ea4abf8f3f2f25736012c
Sam Stiller <sstiller6>
2026-02-03 09:50:58 -0500

    a lot

--- public/apple-touch-icon.png.tsx
+++
@@ -1,3 +0,0 @@
-<!-- This is a placeholder - you'll need to generate actual PNG files -->
-<!-- Use an online tool like https://realfavicongenerator.net/ to convert the SVG -->
-<!-- Or use the SVG as a starting point in design tools -->
